2917294d7535b59df29e186c2c357bb72beea57e
Matthew Shen <37197710+flyme2bluemoon@users.noreply.github.com>
2021-05-12 00:05:53 -0400

    Added some error checking to submission handler

--- src/submissionHandler.ts
+++ src/submissionHandler.ts
@@ -10,23 +10,25 @@ export function submissionHandler(language: string, sourceCodeFile: string, inpu
         outputFilename = getTempOutFilename();
     }
     let runCommand = "";
-    if (language === "c") {
+    if (language === "C") {
         runCommand += `cc ${sourceCodeFile} -o ${tempFilename}`;
         cleanUpNeeded = true;
         runCommand += ` && ${inputFile ? `cat ${inputFile} |` : ""} ./${tempFilename} ${outputTest ? `| tee ${outputFilename}` : ""}`;
-    } else if (language === "cpp") {
+    } else if (language === "C++") {
         runCommand += `c++ ${sourceCodeFile} -std=c++17 -o ${tempFilename}`;
         cleanUpNeeded = true;
         runCommand += ` && ${inputFile ? `cat ${inputFile} |` : ""} ./${tempFilename} ${outputTest ? `| tee ${outputFilename}` : ""}`;
-    } else if (language === "py") {
+    } else if (language === "Python") {
         runCommand += `${inputFile ? `cat ${inputFile} |` : ""} python3 ${sourceCodeFile} ${outputTest ? `| tee ${outputFilename}` : ""}`;
-    } else if (language === "swift") {
+    } else if (language === "Swift") {
         runCommand +=`swiftc ${sourceCodeFile} -o ${tempFilename}`;
         cleanUpNeeded = true;
         runCommand += ` && ${inputFile ? `cat ${inputFile} |` : ""} ./${tempFilename} ${outputTest ? `| tee ${outputFilename}` : ""}`;
+    } else {
+        vscode.window.showWarningMessage("An error has occured. Please submit an issue at https://github.com/flyme2bluemoon/challenger/issues. Sorry for the inconvenience...")
     }
     if (outputTest) {
-        runCommand += ` && diff ${outputTest} ${outputFilename} && rm ${outputFilename}`;
+        runCommand += ` && diff ${outputTest} ${outputFilename}; rm ${outputFilename}`;
     }
     if (cleanUpNeeded) {
         runCommand += ` && rm ${tempFilename}`;
